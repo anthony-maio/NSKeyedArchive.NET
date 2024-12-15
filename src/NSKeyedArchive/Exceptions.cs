592530e6665b6023ce26333189ab14e593470537
Anthony Maio <anthony21@gmail.com>
2024-12-14 22:37:16 -0500

    Initial comment: NSKeyedArchive.NET Implementation

--- src/NSKeyedArchive/Exceptions.cs
+++ src/NSKeyedArchive/Exceptions.cs
@@ -1,42 +1,62 @@
+using System;
+using NSKeyedArchive;
+using System.Reflection;
+
 namespace NSKeyedArchive
 {
     /// <summary>
     /// Exception thrown when there is an error processing a property list.
     /// </summary>
-    // TODO : flesh this out, make this abstract, possible capture information on what happened.
     public class PListException : Exception
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="PListException"/> class with a specified error message.
+        /// Initializes a new instance of the <see cref="PListException"/> class.
         /// </summary>
-        /// <param name="message">The message that describes the error.</param>
-        public PListException(string message) : base(message) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PListException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
-        /// </summary>
-        /// <param name="message">The message that describes the error.</param>
-        /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
+        /// <param name="message">The error message.</param>
+        /// <param name="inner">The inner exception.</param>
         public PListException(string message, Exception inner) : base(message, inner) { }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PListException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public PListException(string message) : base(message) { }
     }
 
     /// <summary>
-    /// Exception thrown when there is a format error in a property list.
+    /// Exception thrown when there is an error in the format of a property list.
     /// </summary>
     public class PListFormatException : PListException
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="PListFormatException"/> class with a specified error message.
+        /// Initializes a new instance of the <see cref="PListFormatException"/> class.
         /// </summary>
-        /// <param name="message">The message that describes the error.</param>
+        /// <param name="message">The error message.</param>
         public PListFormatException(string message) : base(message) { }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PListFormatException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+        /// Initializes a new instance of the <see cref="PListFormatException"/> class.
         /// </summary>
-        /// <param name="message">The message that describes the error.</param>
-        /// <param name="inner">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
+        /// <param name="message">The error message.</param>
+        /// <param name="inner">The inner exception.</param>
         public PListFormatException(string message, Exception inner) : base(message, inner) { }
     }
-}
 
+    /// <summary>
+    /// Exception thrown when there is an error unarchiving an NSKeyedArchiver plist.
+    /// </summary>
+    public class NSKeyedUnarchiverException : PListException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NSKeyedUnarchiverException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public NSKeyedUnarchiverException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PListFormatException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="inner">The inner exception.</param>
+        public NSKeyedUnarchiverException(string message, Exception inner) : base(message, inner) { }
+     }
+}
